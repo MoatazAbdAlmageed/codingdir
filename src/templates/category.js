f9c4d9bc619df93ddadf01f4e82513838e1de543
Moataz Mohammady <moataz@wuilt.com>
2021-08-14 14:59:19 +0200

    fix toUpperCase

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../layout'
-import PostListing from '../components/PostListing'
-import config from '../../data/SiteConfig'
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../layout";
+import PostListing from "../components/PostListing";
+import config from "../../data/SiteConfig";
 
 const CategoryTemplate = ({ data, pageContext }) => (
   <Layout>
     <main>
       <Helmet title={` "${pageContext.category}" - ${config.siteTitle}`} />
-      <h1>
-Category:
-{' '}
-{pageContext.category}
-</h1>
+      <h1>Category: {pageContext.category.toUpperCase()}</h1>
       <PostListing postEdges={data.allMarkdownRemark.edges} />
     </main>
   </Layout>
-)
+);
 
-export default CategoryTemplate
+export default CategoryTemplate;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
@@ -49,4 +45,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
