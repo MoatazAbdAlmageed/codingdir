b8455a7b1ef8133f903627f9a31ccb6862c50866
Moataz Mohammady <moataz@wuilt.com>
2021-08-19 22:49:00 +0200

    wip

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import config from '../../data/SiteConfig'
-import styles from './index.module.scss'
+import React from "react";
+import Helmet from "react-helmet";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import config from "../../data/SiteConfig";
 
 const MainLayout = ({ children }) => (
   <>
     <Header />
+    <meta
+      name="google-site-verification"
+      content="Uyx7zn5ADHSsjqw3VQQ5opqVl3XmgkMHKEYPF3ZCpPw"
+    />
     <Helmet>
       <meta name="description" content={config.siteDescription} />
     </Helmet>
     {children}
     <Footer />
   </>
-)
+);
 
-export default MainLayout
+export default MainLayout;
