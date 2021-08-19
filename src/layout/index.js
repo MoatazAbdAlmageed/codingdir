9f4b8571cb27f474daa8fac6208eba50f0e1d955
Moataz Mohammady <moataz@wuilt.com>
2021-08-19 23:09:27 +0200

    restore styles

--- src/layout/index.js
+++ src/layout/index.js
@@ -3,11 +3,11 @@ import Helmet from "react-helmet";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import config from "../../data/SiteConfig";
+import styles from "./index.module.scss";
 
 const MainLayout = ({ children }) => (
   <>
     <Header />
-
     <Helmet>
       <meta name="description" content={config.siteDescription} />
       <meta
