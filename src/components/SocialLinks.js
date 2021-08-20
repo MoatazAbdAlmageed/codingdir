e80b25bb5658c37dc69789b1734e3331f4862061
Moataz Mohammady <moataz@wuilt.com>
2021-08-20 21:34:36 +0200

    remove Reddit

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,47 +1,38 @@
-import React from 'react'
+import React from "react";
 import {
-  FacebookShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  TelegramShareButton,
-  RedditShareButton,
-  FacebookShareCount,
-  LinkedinShareCount,
-  RedditShareCount,
   FacebookIcon,
-  TwitterIcon,
-  TelegramIcon,
+  FacebookShareButton,
+  FacebookShareCount,
   LinkedinIcon,
-  RedditIcon
-} from 'react-share'
-import urljoin from 'url-join'
-import config from '../../data/SiteConfig'
-import styles from './SocialLinks.module.scss'
+  LinkedinShareButton,
+  LinkedinShareCount,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
+import urljoin from "url-join";
+import config from "../../data/SiteConfig";
+import styles from "./SocialLinks.module.scss";
 
 const SocialLinks = ({ postNode, postPath, mobile }) => {
-  const post = postNode.frontmatter
-  const url = urljoin(config.siteUrl, config.pathPrefix, postPath)
-  const iconSize = mobile ? 32 : 32
-  const filter = count => (count > 0 ? count : '')
-  const renderShareCount = count => (
+  const post = postNode.frontmatter;
+  const url = urljoin(config.siteUrl, config.pathPrefix, postPath);
+  const iconSize = mobile ? 32 : 32;
+  const filter = (count) => (count > 0 ? count : "");
+  const renderShareCount = (count) => (
     <div className={styles.shareCount}>{filter(count)}</div>
-  )
+  );
 
   return (
     <div className={styles.socialLinks}>
-      <RedditShareButton url={url} title={post.title}>
-        <RedditIcon round size={iconSize} />
-        <RedditShareCount url={url}>
-          {count => renderShareCount(count)}
-        </RedditShareCount>
-      </RedditShareButton>
       <TwitterShareButton url={url} title={post.title}>
         <TwitterIcon round size={iconSize} />
       </TwitterShareButton>
       <FacebookShareButton url={url} quote={postNode.excerpt}>
         <FacebookIcon round size={iconSize} />
         <FacebookShareCount url={url}>
-          {count => renderShareCount(count)}
+          {(count) => renderShareCount(count)}
         </FacebookShareCount>
       </FacebookShareButton>
       <LinkedinShareButton
@@ -51,14 +42,14 @@ const SocialLinks = ({ postNode, postPath, mobile }) => {
       >
         <LinkedinIcon round size={iconSize} />
         <LinkedinShareCount url={url}>
-          {count => renderShareCount(count)}
+          {(count) => renderShareCount(count)}
         </LinkedinShareCount>
       </LinkedinShareButton>
       <TelegramShareButton url={url}>
         <TelegramIcon round size={iconSize} />
       </TelegramShareButton>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
