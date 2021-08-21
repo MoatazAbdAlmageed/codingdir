65127c053ca4af5413df7f4b8e8df68ac016e1aa
Moataz Mohammady <moataz@wuilt.com>
2021-08-21 05:06:20 +0200

    add searchBox

--- src/components/PostListing.js
+++ src/components/PostListing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import styles from "./PostsListing.module.scss";
+import uuid from "react-uuid";
 
 const PostListing = ({ postEdges }) => {
   const getPostList = () => {
@@ -21,24 +22,69 @@ const PostListing = ({ postEdges }) => {
   };
 
   const postList = getPostList();
+
+  const emptyQuery = "";
+
+  const [state, setState] = React.useState({
+    query: emptyQuery,
+    filteredData: [],
+  });
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    const query = event.target.value;
+    const filteredData = postList.filter(({ excerpt, title, tags }) => {
+      const low = query.toLowerCase();
+      return (
+        excerpt.toLowerCase().includes(low) ||
+        title.toLowerCase().includes(low) ||
+        (tags &&
+          tags
+            .join("")
+            .toLowerCase()
+            .includes(low))
+      );
+    });
+
+    setState({
+      query,
+      filteredData,
+    });
+  };
+  const { filteredData, query } = state;
+  const hasSearchResults = filteredData && query;
+  const posts = hasSearchResults ? filteredData : postList;
+
   return (
-    <div className={styles.articleList}>
-      {/* Your post list here. */
-      postList.map((post) => (
-        <Link className={styles.articleBox} to={post.path} key={post.title}>
-          <article>
-            <div className={styles.right}>
-              <h3>{post.title}</h3>
-              <img src={post.cover} />
-              <div className={styles.meta}>
-                {post.date} &mdash;{" "}
-                <span>{post.categories && post.categories.join(" / ")}</span>{" "}
+    <div>
+      <div className={styles.searchBox}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          aria-label="Search"
+          placeholder="Search by title, tags ,excerpt"
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className={styles.articleList}>
+        {/* Your post list here. */
+        posts.map((post) => (
+          <Link className={styles.articleBox} to={post.path} key={uuid()}>
+            <article>
+              <div className={styles.right}>
+                <h3>{post.title}</h3>
+                <img src={post.cover} />
+                <div className={styles.meta}>
+                  {post.date} &mdash;{" "}
+                  <span>{post.categories && post.categories.join(" / ")}</span>{" "}
+                </div>
+                <p>{post.excerpt}</p>
               </div>
-              <p>{post.excerpt}</p>
-            </div>
-          </article>
-        </Link>
-      ))}
+            </article>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
