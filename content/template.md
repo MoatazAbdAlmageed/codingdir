0e082c450ec5cbfe870ab3e2a22838818d4afbfa
Moataz <moataz@wuilt.com>
2022-01-06 09:48:15 +0200

    update submit channel

---
+++ content/template.md
@@ -0,0 +1,14 @@
+---
+title: "Jamie Barton"
+date: "2020-08-16"
+categories:
+  - "english"
+tags:
+  - "graphql"
+  - "nuxtjs"
+  - "gatsby"
+  - "nodejs"
+cover: "https://yt3.ggpht.com/ytc/AKedOLRqNyy2HI9vLhxFkk4Z2kEJHAkZO-Cn8eNBkydukQ=s176-c-k-c0x00ffffff-no-rj"
+---
+
+https://www.youtube.com/c/JamieBartonCodes/playlists
--- src/pages/submit_channel.js
+++ src/pages/submit_channel.js
@@ -7,16 +7,25 @@ const SubmitChannel = () => (
   <Layout>
     <main>
       <Helmet title={`Submit Channel | ${config.siteTitle}`} />
-      <h1>
+      <h1>Submit Channel</h1>
+
+      <p>
         If you need to submit your channel or another useful one please submit a
         pull request to project repo here
-        https://github.com/MoatazAbdAlmageed/YouTube-Channels steps
-      </h1>
-      <p>
-        If you don't know how to contribute kindly check this awesome tutorial
-        here First check this tutorial here
-        https://www.youtube.com/watch?v=G1I3HF4YWEw
+        https://github.com/MoatazAbdAlmageed/YouTube-Channels
       </p>
+
+      <h2>Steps</h2>
+      <ul>
+        <li>
+          Fork the repo If you don't know how to contribute kindly check this
+          awesome tutorial here First check this tutorial here
+          https://www.youtube.com/watch?v=G1I3HF4YWEw
+        </li>
+        <li>duplicate .\content\template.md file with channel name</li>
+        <li>Update file with channel name , tags , link , ...</li>
+        <li>Send a pull request</li>
+      </ul>
     </main>
   </Layout>
 );
