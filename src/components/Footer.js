b4691717f1f57d10395251d7be58e9f329363553
Moataz <moataz@wuilt.com>
2022-03-09 23:39:46 +0200

    enhance footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,18 @@
-import {
-  ButtonGroup,
-  Container,
-  IconButton,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Container, Stack, Text } from "@chakra-ui/react";
+import { Link } from "gatsby";
 import * as React from "react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import config from "../../data/SiteConfig";
 
 const Footer = () => (
   <Container as="footer" role="contentinfo" py={{ base: "12", md: "16" }}>
     <Stack spacing={{ base: "4", md: "5" }}>
-      <Stack justify="space-between" direction="row" align="center">
-        <ButtonGroup variant="ghost">
-          <IconButton
-            as="a"
-            target="_blank"
-            href={config.userLinkedin}
-            aria-label="LinkedIn"
-            icon={<FaLinkedin fontSize="1.25rem" />}
-          />
-          <IconButton
-            as="a"
-            target="_blank"
-            href={config.gitHub}
-            aria-label="GitHub"
-            icon={<FaGithub fontSize="1.25rem" />}
-          />
-          <IconButton
-            as="a"
-            target="_blank"
-            href={config.userTwitter}
-            aria-label="Twitter"
-            icon={<FaTwitter fontSize="1.25rem" />}
-          />
-        </ButtonGroup>
-      </Stack>
       <Text fontSize="sm" color="subtle">
+        <Link
+          style={{ color: "orange" }}
+          target="_blank"
+          to="/moataz-mohammady"
+        >
+          By Moataz Mohammady
+        </Link>{" "}
         &copy; {new Date().getFullYear()} Chakra UI Pro, Inc. All rights
         reserved.
       </Text>
