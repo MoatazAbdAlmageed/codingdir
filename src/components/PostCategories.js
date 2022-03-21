ebcdd980a47d519578bafbda6689daedde602596
Moataz <moataz@wuilt.com>
2022-03-21 23:36:45 +0200

    enhance categories

--- src/components/PostCategories.js
+++ src/components/PostCategories.js
@@ -4,21 +4,21 @@ import _ from "lodash";
 import React from "react";
 const PostCategories = ({ categories, margin }) => {
   return (
-    <HStack marginBottom={2}>
-      <Tag
-        style={{ background: "orange" }}
-        size={"md"}
-        variant="solid"
-        colorScheme="blue"
-        margin={margin}
-      >
-        {categories &&
-          categories.map((category) => (
+    <HStack marginBottom={1}>
+      {categories &&
+        categories.map((category) => (
+          <Tag
+            style={{ background: "orange" }}
+            size={"md"}
+            variant="solid"
+            colorScheme="blue"
+            margin={margin}
+          >
             <Link key={category} to={`/${_.kebabCase(category)}`}>
               <span>{category.toUpperCase()}</span>
             </Link>
-          ))}
-      </Tag>
+          </Tag>
+        ))}
     </HStack>
   );
 };
