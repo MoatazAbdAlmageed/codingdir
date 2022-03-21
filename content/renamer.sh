fc97e627a873ae54dc6680f05431a370ad7424c2
Moataz Mohammady <moataz@wuilt.com>
2022-03-21 20:41:19 +0200

    wip

--- content/renamer.sh
+++ content/renamer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 for filename in *.md; do
-newFileName=`sed -n '2p' < $filename | sed -e 's/title://g'  -e "s/\"//g"   -e 's/^"//' -e 's/"$//'   ` 
-echo "$newFileName.md"
-mv $filename  "$newFileName.md"
+newFileName=`sed -n '2p' < $filename  |  tr '\n' '\r' | sed -e 's/*$ *$//g'   -e 's/title://g'  -e "s/\ //g"  -e "s/\-//g"   -e 's//-/g' -e 's//  |  /g'   -e 's/|//g' -e 's/^|//' -e 's/"$//' -e "s/\"//g"   -e 's/^"//' -e 's/"$//' | tr '\r' '\n' `
+# echo $filename
+name=$newFileName.md
+echo $name
+mv $filename  $name
 done
