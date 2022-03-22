62a49cf06e60797bcd8e26c550693c2df7ba7291
Moataz Mohammady <moataz@wuilt.com>
2022-03-22 11:57:07 +0200

    update me link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import { Container, Text } from "@chakra-ui/react";
-import { Link } from "gatsby";
 import * as React from "react";
 
 const Footer = () => (
   <Container as="footer" role="contentinfo">
     <Text fontSize="sm" color="subtle">
-      <Link style={{ color: "orange" }} target="_blank" to="/moataz-mohammady">
+      <a
+        style={{ color: "orange" }}
+        target="_blank"
+        href="https://moatazabdalmageed.github.io/"
+      >
         By Moataz Mohammady
-      </Link>{" "}
+      </a>{" "}
       &copy; {new Date().getFullYear()} Built with ♥ using{" "}
       <a href="https://www.gatsbyjs.com/" target="_blank">
         Gatsby
