2c0a7a6666dba9d9b57e53cdd73f84a6bf58056c
Moataz <moataz@wuilt.com>
2022-05-04 01:30:20 +0200

    add dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 const urljoin = require("url-join");
 const config = require("./data/SiteConfig");
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   pathPrefix: config.pathPrefix === "" ? "/" : config.pathPrefix,
   siteMetadata: {
