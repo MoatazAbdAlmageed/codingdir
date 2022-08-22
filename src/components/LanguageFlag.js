ad93ab9638e9b076ca4b4a7366c841918d51247c
Moataz <moataz@wuilt.com>
2022-08-22 16:47:56 +0200

    fix lang flags

--- src/components/LanguageFlag.js
+++ src/components/LanguageFlag.js
@@ -2,12 +2,15 @@ import { Link, Text } from "@chakra-ui/react";
 import React from "react";
 
 export function LanguageFlag({ post }) {
-  if (!post?.categories) return;
-  return post?.categories?.map((category) => (
-    <Text m={(0, 2)}>
-      <Link to={`/${category}}`}>
-        {category.toLowerCase() === "english" ? "🇬🇧" : "🇸🇦"}
-      </Link>
-    </Text>
-  ));
+  return !post?.categories ? (
+    <></>
+  ) : (
+    post?.categories?.map((category) => (
+      <Text m={(0, 2)}>
+        <Link to={`/${category}}`}>
+          {category.toLowerCase() === "english" ? "🇬🇧" : "🇸🇦"}
+        </Link>
+      </Text>
+    ))
+  );
 }
