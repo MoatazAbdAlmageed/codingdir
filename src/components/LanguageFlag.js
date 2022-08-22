36e8895b1d87e8682c4d3fcdfa9425e79731420b
Moataz <moataz@wuilt.com>
2022-08-22 16:44:49 +0200

    fix lang flags

--- src/components/LanguageFlag.js
+++ src/components/LanguageFlag.js
@@ -2,6 +2,7 @@ import { Link, Text } from "@chakra-ui/react";
 import React from "react";
 
 export function LanguageFlag({ post }) {
+  if (!post?.categories) return;
   return post?.categories?.map((category) => (
     <Text m={(0, 2)}>
       <Link to={`/${category}}`}>
