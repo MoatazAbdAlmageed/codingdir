393f7c2923ae07a11e6b66291949f2226e72a94b
Moataz <moataz@wuilt.com>
2022-08-25 18:28:04 +0200

    update siteDescription

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -5,8 +5,7 @@ const config = {
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://youtubechannels.gatsbyjs.io/", // Domain of your website without pathPrefix.
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription:
-    "A Gatsby Markdown Blog stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "YouTube Coding Channels", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   googleAnalyticsID: "UA-205524940-1", // GA tracking ID.
