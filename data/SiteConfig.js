02673634370fe4dad87af417ce5613f1fb4faa0d
Moataz Mohammady <moataz.mohammady@gmail.com>
2026-03-23 11:49:28 +0200

    feat: Add GitHub Pages deployment workflow and initial site configuration.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,8 +3,8 @@ const config = {
   siteTitleShort: "Coding Dir", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Coding Dir", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://moatazabdalmageed.github.io/codingdir", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteUrl: "https://moatazabdalmageed.github.io", // Domain of your website without pathPrefix.
+  pathPrefix: "/codingdir", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Coding Directory", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
